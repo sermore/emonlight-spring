50356b6394d09d61ec607b0cdcc259142e93663c
Sergio Moretti <sergio@reliqs.net>
2018-02-20 23:19:13 +0100

    Added JpaRepositories annotation to JpaServiceConfiguration class.

--- xbee-gw/src/main/java/net/reliqs/emonlight/xbeegw/send/jpa/JpaServiceConfiguration.java
+++ xbee-gw/src/main/java/net/reliqs/emonlight/xbeegw/send/jpa/JpaServiceConfiguration.java
@@ -7,10 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ConditionalOnProperty(name = "jpa.enabled")
 @Configuration
-//@EnableJpaRepositories("net.reliqs.emonlight.xbeegw.send.jpa")
+@EnableJpaRepositories("net.reliqs.emonlight.xbeegw.send.jpa")
 public class JpaServiceConfiguration {
     private static final Logger log = LoggerFactory.getLogger(JpaServiceConfiguration.class);
 
